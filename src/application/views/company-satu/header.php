68d9c6edf53eff685e1b19016367b50e10d7eb4c
yanto <mrthx.89@gmail.com>
2021-11-09 09:21:03 +0700

    telp

--- src/application/views/company-satu/header.php
+++ src/application/views/company-satu/header.php
@@ -3,17 +3,17 @@
         <div class="header-container container">
             <div class="header-row">
                 <div class="header-column">
-                    <div class="header-logo"> 
-						<?php 
-							$logo = $this->model_utama->view_ordering_limit('logo','id_logo','DESC',0,1);
-							foreach ($logo->result_array() as $row) {
-							echo "<img src='".base_url()."asset/logo/$row[gambar]'/>";
-							}
-							?>
-						
+                    <div class="header-logo">
+                        <?php
+                        $logo = $this->model_utama->view_ordering_limit('logo', 'id_logo', 'DESC', 0, 1);
+                        foreach ($logo->result_array() as $row) {
+                            echo "<img src='" . base_url() . "asset/logo/$row[gambar]'/>";
+                        }
+                        ?>
+
                     </div>
                 </div>
-				<div class="header-column">
+                <div class="header-column">
                     <div class="header-search">
                         <form id="searchForm" action="<?php echo base_url(); ?>berita/index/" method="POST">
                             <div class="input-group">
@@ -25,7 +25,7 @@
                         </form>
                     </div>
                 </div>
-                
+
             </div>
         </div>
         <div class="header-container header-nav header-nav-center header-nav-bar header-nav-bar-primary">
@@ -41,36 +41,36 @@
                                 <i class="fa fa-home" style="font-size:25px;"></i>
                             </a>
                         </li>
-						        <?php include "main_menu.php"; ?>
-                                        <a href="<?php echo base_url();?>hubungi/">
-                                            Hubungi Kami
-                                        </a>
+                        <?php include "main_menu.php"; ?>
+                        <a href="<?php echo base_url(); ?>hubungi/">
+                            Hubungi Kami
+                        </a>
 
-                                    
-                                </li>
+
+                        </li>
                     </ul>
                 </nav>
             </div>
 
 
-</script>
+            </script>
 
-<a href="https://api.whatsapp.com/send?phone=6285258454600&amp;text=Halo%20PTPLR,%20Saya%20mau%20order%20Solar%20Industri">
-<img src="asset\img_galeri/whatsapp-kontak.gif" class="wabutton" alt="WhatsApp-Button">
+            <a href="https://api.whatsapp.com/send?phone=6285733988103&amp;text=Halo%20PTPLR,%20Saya%20mau%20order%20Solar%20Industri">
+                <img src="asset\img_galeri/whatsapp-kontak.gif" class="wabutton" alt="WhatsApp-Button">
 
-</a>
-<style>
-.wabutton{
-position:fixed;
-bottom:20px;
-left: :20px;
-z-index:100;
-width: 240px;
-}
-</style>
+            </a>
+            <style>
+                .wabutton {
+                    position: fixed;
+                    bottom: 20px;
+                    left: :20px;
+                    z-index: 100;
+                    width: 240px;
+                }
+            </style>
 
 
-</script>
+            </script>
 
 
 
